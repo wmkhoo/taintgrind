ecebc4b6e5c83c61ee069252cca8de046b59344a
Wei Ming Khoo <wmkhoo@github.com>
2015-04-01 20:32:58 +0800

    bugfix: increase TI_MAX to 700

--- tnt_main.c
+++ tnt_main.c
@@ -2455,7 +2455,7 @@ Int atoi( HChar *s ){
 // tmp variables go from t0, t1, t2,..., t255
 // reg variables go from r0, r4, r8,..., r320
 // see libvex_guest_amd64.h
-#define TI_MAX 440
+#define TI_MAX 700
 #define RI_MAX 740 
 // These arrays are initialised to 0 in TNT_(clo_post_init)
 // Tmp variable indices; the MSB indicates whether it's tainted (1) or not (0)
