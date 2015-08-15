b80271fafd2303b646114bb39dbd2990de233439
Wei Ming Khoo <weimzz@gmail.com>
2015-08-15 11:54:31 +0800

    tests: sign8/16/32/64

---
+++ tests/sign32.c
@@ -0,0 +1,18 @@
+#include "taintgrind.h"
+int get_sign(int x) {
+    if (x == 0) return 0;
+    if (x < 0)  return -1;
+    return 1;
+}
+int main(int argc, char **argv)
+{
+    // Turns on printing
+    TNT_START_PRINT();
+    int a = 1000;
+    // Defines int a as tainted
+    TNT_MAKE_MEM_TAINTED(&a,4);
+    int s = get_sign(a);
+    // Turns off printing
+    TNT_STOP_PRINT();
+    return s;
+}
