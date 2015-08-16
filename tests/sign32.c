a77c2db3ca4e998d919014a5f89a4d13f22345eb
Wei Ming Khoo <weimzz@gmail.com>
2015-08-16 16:46:41 +0800

    use make_mem_tainted_named

--- tests/sign32.c
+++ tests/sign32.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     TNT_START_PRINT();
     int a = 1000;
     // Defines int a as tainted
-    TNT_MAKE_MEM_TAINTED(&a,4);
+    TNT_MAKE_MEM_TAINTED_NAMED(&a,4, "myint");
     int s = get_sign(a);
     // Turns off printing
     TNT_STOP_PRINT();
