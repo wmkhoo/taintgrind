9bdd270350878723c030f4d414f9870411f9a143
Khoo Wei Ming <kweiming@dso.org.sg>
2017-03-15 17:25:03 +0800

    Tests for reading stdin

---
+++ tests/signstdin2.c
@@ -0,0 +1,30 @@
+//#include "taintgrind.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int get_sign(int x){
+	if (x == 0){
+		return 0;
+	}
+	if (x < 0){
+		return -1;
+	}
+	return 1;
+}
+
+int main(int argc, char **argv){
+
+	// Turns on printing
+	TNT_START_PRINT();
+
+	int a = atoi(argv[1]);
+
+	//Defines int as tainted
+	TNT_MAKE_MEM_TAINTED_NAMED(&a,4,"myint");
+	int s = get_sign(a);
+
+	// Turns off printing
+	TNT_STOP_PRINT();
+	return s;
+}
