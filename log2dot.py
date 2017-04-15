013fffe79aa9c887c69251010cc8380c85098d44
wmkhoo <weimzz@gmail.com>
2017-04-15 11:42:32 +0800

    Graph visualisation: Deal with multiple flows in 1 insn

--- log2dot.py
+++ log2dot.py
@@ -30,6 +30,8 @@ def resolve_unknown_var(varname, line):
         if addr in var:
             return var[addr]
 
+    varname = varname.replace('[','_').replace(']','_')
+
     if re.match('^[0-9]',varname):
         return 'g' + varname
     return varname
@@ -39,6 +41,7 @@ def get_op(line):
     if "STORE" in line:    return "STORE"
     if "IF" in line:       return "IF"
     if line.split(" | ")[1][0] == 'r':  return "PUT"
+    if line.split(" = ")[1].split()[0][0] == 'r': return "GET"
     if line.split(" = ")[1].split()[0][0] == 't': return "RDTMP"
     return line.split(" = ")[1].split()[0]
 
@@ -47,6 +50,7 @@ ops_to_skip = ["to1",
                "to16",
                "to32",
                "to64",
+               "GET",
                "PUT",
                "RDTMP",
                "LOAD",
@@ -108,10 +112,16 @@ while line:
     if "LOAD" in insn and "_unknownobj" not in line and len(flow) >= 4:
         addr = get_load_or_store_addr(line)
         if addr not in var:
-            if "<- " in flow:
-                var[addr] = flow.rstrip().split("<- ")[1]
-            elif "<*- " in flow:
-                var[addr] = flow.rstrip().split("<*- ")[1]
+            if ";" in flow:
+                flows = flow.split("; ")
+            else:
+                flows = [flow]
+
+            for flow in flows:
+                if "<- " in flow:
+                    var[addr] = flow.rstrip().split("<- ")[1]
+                elif "<*- " in flow:
+                    var[addr] = flow.rstrip().split("<*- ")[1]
 
     line = f.readline()
 
@@ -124,39 +134,50 @@ for line in data:
 
     # If there is taint flow
     if len(line.split(" | ")[-1]) >= 4:
-        flow = line.split(" | ")[-1].rstrip()
+        flow_orig = line.split(" | ")[-1].rstrip()
 
-        if "<-" in flow or "<*-" in flow:
-            sink = ""
-            source = ""
-
-            if " <- " in flow:
-                (sink,source) = flow.split(" <- ")
-            elif " <*- " in flow:
-                (sink,source) = flow.split(" <*- ")
-
-            if "LOAD" in line:
-                source = resolve_unknown_var(source, line)
-            elif "STORE" in line:
-                sink = resolve_unknown_var(sink, line)
-
-            src = []
-            if ", " in source:
-                src = source.split(", ")
+        if "<-" in flow_orig or "<*-" in flow_orig:
+            if ";" in flow_orig:
+                flows = flow_orig.split("; ")
             else:
-                src.append(source)
+                flows = [flow_orig]
+
+            for flow in flows:
+                sink = ""
+                source = ""
+
+                if " <- " in flow:
+                    (sink,source) = flow.split(" <- ")
+                elif " <*- " in flow:
+                    (sink,source) = flow.split(" <*- ")
+
+                if "LOAD" in line:
+                    source = resolve_unknown_var(source, line)
+                elif "STORE" in line:
+                    sink = resolve_unknown_var(sink, line)
+
+                src = []
+                if ", " in source:
+                    src = source.split(", ")
+                else:
+                    src.append(source)
  
-            for source in src:
-                edges.append("%s -> %s" % (source,sink))
+                for source in src:
+                    edges.append("%s -> %s" % (source,sink))
 
-                if source not in nodes:
-                    nodes[source] = source
+                    if source not in nodes:
+                        nodes[source] = source
 
-            if sink not in nodes:
-                nodes[sink] = sink
+                if sink not in nodes:
+                    nodes[sink] = sink
 
-            if not op_can_be_skipped(line):
-                nodes[sink] = "%s [label=\"%s (%s)\"]" % (sink,sink,get_op(line))
+                if not op_can_be_skipped(line):
+                    if ";" in flow_orig:
+                        nodes[sink] = "%s [label=\"%s (%s)\",color=red]" % (sink,sink,get_op(line))
+                    else:
+                        nodes[sink] = "%s [label=\"%s (%s)\"]" % (sink,sink,get_op(line))
+                elif ";" in flow_orig:
+                    nodes[sink] = "%s [color=red]" % (sink)
         else:
             nodes[sink] = "%s [label=\"%s (%s)\"]" % (sink,sink,get_op(line))
 
@@ -171,24 +192,30 @@ for line in data:
         flow = line.split(" | ")[-1].rstrip()
 
         if "<-" in flow or "<*-" in flow:
-            sink = ""
-            source = ""
+            if ";" in flow:
+                flows = flow.split("; ")
+            else:
+                flows = [flow]
 
-            if " <- " in flow:
-                (sink,source) = flow.split(" <- ")
-            elif " <*- " in flow:
-                (sink,source) = flow.split(" <*- ")
+            for flow in flows:
+                sink = ""
+                source = ""
 
-            if "LOAD" in line:
-                source = resolve_unknown_var(source, line)
-            elif "STORE" in line:
-                sink = resolve_unknown_var(sink, line)
+                if " <- " in flow:
+                    (sink,source) = flow.split(" <- ")
+                elif " <*- " in flow:
+                    (sink,source) = flow.split(" <*- ")
 
-            loc = get_loc(line)
+                if "LOAD" in line:
+                    source = resolve_unknown_var(source, line)
+                elif "STORE" in line:
+                    sink = resolve_unknown_var(sink, line)
+
+                loc = get_loc(line)
     
-            if loc not in subgraph:
-                subgraph[loc] = ""
-            subgraph[loc] += "    %s\n" % (nodes[sink])
+                if loc not in subgraph:
+                    subgraph[loc] = ""
+                subgraph[loc] += "    %s\n" % (nodes[sink])
         else:
 
             loc = get_loc(line)
