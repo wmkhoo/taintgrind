babd03b53492e5e28b2d7fabd3e04aedee9df21d
Khoo Wei Ming <kweiming@dso.org.sg>
2018-02-27 12:36:12 +0800

    Detecting a buffer overflow(#17): testcase

---
+++ tests/misprint.c
@@ -0,0 +1,57 @@
+/* misprint.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include "taintgrind.h"
+
+void hello_function()
+{
+    printf("INFO: Hello World!\n");
+}
+
+/* 
+ * $ VAL=`readelf -s misprint | grep secret_function | awk '{print $2}'` && printf "%d\n" 0x$VAL
+ */
+void secret_function()
+{
+    printf("INFO: Oh no! The application is compromised!\n");
+}
+
+int main(int argc, char** argv)
+{
+
+    int i;
+    int val;
+    int iter;
+    int buffer[10];
+    int canary;
+    void (*func)(void);
+
+    if (argc != 3) { return(1); }
+
+    iter = atoi(argv[1]);
+    val = atoi(argv[2]);
+
+    TNT_MAKE_MEM_TAINTED( &iter, sizeof(iter));
+    TNT_MAKE_MEM_TAINTED( &val, sizeof(val));
+    //TNT_START_PRINT();
+
+    /* It should print Hello World! */
+    func = &hello_function; 
+
+    canary = 123; //UNINTIALIZED;
+
+    /* If iter > 10 then a buffer overflow will occur */
+    for (i = 0; i < iter; i++)
+        buffer[i] = val;
+
+    /* 
+     * ... if the buffer overflow is "severe enough", 
+     * it may overwrite the function pointer with user data, 
+     * e.g. a pointer to the secret function
+     */
+    func();
+
+    //TNT_STOP_PRINT();
+
+    return(0);
+}
