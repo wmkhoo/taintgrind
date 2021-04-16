328087f8f8dd3adb97cf1045a4f962442d4bfc69
Wei Ming Khoo <weimzz@gmail.com>
2021-04-16 13:30:21 +0800

    Simplify building capstone

--- build_taintgrind.sh
+++ build_taintgrind.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Patch valgrind-3.13
+# Patch valgrind
 patch -d ../ -p0 < d3basics.patch
 
 # Find out how far we can parallelize the build
@@ -14,10 +14,6 @@ cd ../ && \
     ./configure --prefix=`pwd`/build && \
     make -j"$jobs" && \
     make install
-
-# grep the Makefile to get VGCONF_ARCH_PRI and VGCONF_ARCH_SEC
-VGCONF_ARCH_PRI=$(grep VGCONF_ARCH_PRI Makefile | sed 's/VGCONF_ARCH_PRI = //g')
-VGCONF_ARCH_SEC=$(grep VGCONF_ARCH_SEC Makefile | sed 's/VGCONF_ARCH_SEC = //g')
 
 # Download and extract capstone
 cd taintgrind
@@ -35,26 +31,7 @@ if [ $retval -ne 0 ]; then
        exit
 fi
 
-echo VGCONF_ARCH_PRI
-echo $VGCONF_ARCH_PRI
-if [ -n "$VGCONF_ARCH_PRI" ]; then
-    sh configure_capstone.sh `pwd`/../build $VGCONF_ARCH_PRI && \
-    cd capstone-$CAPSTONE_VERSION && \
-    sh make_capstone.sh && \
-    cd ..
-else
-    echo VGCONF_ARCH_PRI not defined in Makefile
-    exit
-fi
-
-echo VGCONF_ARCH_SEC
-echo $VGCONF_ARCH_SEC
-if [ -n "$VGCONF_ARCH_SEC" ]; then
-    sh configure_capstone.sh `pwd`/../build $VGCONF_ARCH_SEC && \
-    cd capstone-$CAPSTONE_VERSION && \
-    sh make_capstone.sh && \
-    cd ..
-fi
+mv capstone-$CAPSTONE_VERSION capstone
 
 # build taintgrind
 ../autogen.sh && \
